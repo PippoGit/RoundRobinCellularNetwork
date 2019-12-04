0329c4f548ece2dc5cffe5f1b2661ad47d5bdfea
giadaanastasi <40028778+giadaanastasi@users.noreply.github.com>
2019-12-04 17:25:45 +0100

    added packet.msg and user methods

--- src/User.cc
+++ src/User.cc
@@ -4,10 +4,26 @@ Define_Module(User);
 
 void User::initialize()
 {
-    // TODO - Generated method body
+    waitMessage=new cMessage("waitMessage");
+
 }
 
 void User::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+    if(msg->selfMessage()){
+        handleTimer();
+        this->scheduleAt(simTime() + interArrivalTime, waitMessage);
+    }
+
+
+}
+
+void User:: handleTimer(){
+    packet = new Packet();
+    packet->senderID=this->userID;
+    packet->creationTime=simTime();
+    //for assumption: packet dimension between 1 and 75 bytes
+    packet->serviceDemand=omnetpp::intuniform(rng, 1, 76);
+    int numUsers=10; //this->getParentModule()->par("numUsers");
+    packet->receiverID=omnetpp::intuniform(rng, 1, numUser+1);
 }
--- src/User.h
+++ src/User.h
@@ -2,17 +2,21 @@
 #define __ROUNDROBINCELLULARNETWORK_USER_H_
 
 #include <omnetpp.h>
+#include "Packet.msg"
 
 using namespace omnetpp;
 
 class User : public cSimpleModule
 {
   private:
-
-
+    simtime_t interArrivalTime; // exponential
+    cMessage* waitMessage;
+    Packet* packet;
+    int userID;
   protected:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
+    virtual void handleTimer();
 };
 
 #endif
