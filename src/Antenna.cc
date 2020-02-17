4b2f89dd129222a5addf161106603a3cbd6e53b3
giadaanastasi <40028778+giadaanastasi@users.noreply.github.com>
2020-02-17 14:00:55 +0100

    upd

--- src/Antenna.cc
+++ src/Antenna.cc
@@ -38,7 +38,7 @@ void Antenna::initialize()
         pt->setKind(MSG_PKT_TIMER);
         pt->setUserId(i);
         u.setTimer(pt);
-        scheduleAt(simTime() + (i+1)*0.001 /*exponential((simtime_t) par("lambda"), RNG_INTERARRIVAL)*/,pt);
+        scheduleAt(simTime() + /*(i+1)*0.001 */ exponential((simtime_t) par("lambda"), RNG_INTERARRIVAL),pt);
         users.push_back(u);
     }
 
@@ -55,7 +55,7 @@ void Antenna::initialize()
     frame = nullptr;
     numSentBytesPerTimeslot   = 0;
     numServedUsersPerTimeslot = 0;
-    scheduleAt(simTime() + 0.001, timer);
+    scheduleAt(simTime(), timer);
 }
 
 
@@ -70,7 +70,7 @@ void Antenna::initUsersInformation()
     for(std::vector<UserInformation>::iterator it = users.begin(); it != users.end(); ++it)
     {
         double p = (it->getId()<3)? successProbGroup1: (it->getId()<7)? successProbGroup2: successProbGroup3;
-        int cqi = 10; // (isBinomial)?binomial(BINOMIAL_N, p):intuniform(MIN_CQI, MAX_CQI);
+        int cqi = (isBinomial)?binomial(BINOMIAL_N, p):intuniform(MIN_CQI, MAX_CQI);
         EV << "User: " << it->getId() << " - p: " << p << " - cqi: "<<cqi<<endl;
         it->setCQI(cqi);
         it->shouldBeServed();
@@ -235,7 +235,7 @@ void Antenna::createFrame()
 
         // Fill the frame with current user's queue and update currentRB index
         fillFrameWithCurrentUser(currentRB, vframe.end());
-    } while(false/*currentRB != vframe.end()*/);
+    } while(currentRB != vframe.end());
 
     // 3) send the frame to all the users DURING NEXT TIMESLOT!
     this->frame = vectorToFrame(vframe);
@@ -283,7 +283,7 @@ void Antenna::handlePacket(int userId)
     EV_DEBUG << "[UPLINK] Scheduling next packet for User-" << userId << endl;
     simtime_t lambda = par("lambda");
     EV_DEBUG << " LAMBDA: " << lambda << " DOUBLE: " << lambda.dbl() << endl;
-    scheduleAt(simTime() + (userId+1)*0.001 /* exponential(lambda, RNG_INTERARRIVAL)*/, users[userId].getTimer());
+    scheduleAt(simTime() + /*(userId+1)*0.001 */ exponential(lambda, RNG_INTERARRIVAL), users[userId].getTimer());
     EV_DEBUG << "[UPLINK] Done!" << endl;
 }
 
